1ecb9a6e70f56b531163b4016cd669d4b03ff0ad
Nhat Minh <omega.ex1234@gmail.com>
2022-03-16 21:05:24 +0700

    Added Interface ListManagement

---
+++ Assignment1FP/src/rmit/w1/ListManagement.java
@@ -0,0 +1,8 @@
+package rmit.w1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+public interface ListManagement {
+    ArrayList<Student> listOfStudents = new ArrayList<Student>();
+    ArrayList<Course> listOfCourses = new ArrayList<Course>();
+}
